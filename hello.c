c25d8a8483e988e372ef7feb38bb2e6261f40d8d
Sam Price <samuel.r.price@nasa.gov>
2023-10-06 10:52:09 -0400

    Switch to smaller divide test.

--- hello.c
+++ hello.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
-
+#include <stdint.h>
 #include <rtems/shell.h>
 #include <rtems/untar.h>
 
@@ -19,10 +19,8 @@
 #define STRESS_COUNT 100000   // Read 100k times
 
 
-void SetupFileSystem();
-int LSeekStressTest(int lock_interrupts);
+int Int64Test(int lock_interrupts);
 
-int lock_interrupts = 0;
 
 rtems_task Init(
   rtems_task_argument ignored
@@ -32,18 +30,19 @@ rtems_task Init(
     int fails_no_int = 0;
     rtems_status_code sc = RTEMS_SUCCESSFUL;
     printf( "\nStarting interrupt fseek tests \n" );
+    printf("Size of off_t is %d\n",sizeof(off_t));
 
 
-    SetupFileSystem();
     int i = 0;
+    int lock_interrupts;
+    Int64Test(lock_interrupts = 1);
+
     while(1){
 
 
-        fails_int += LSeekStressTest(lock_interrupts = 0);
+        fails_int += Int64Test(lock_interrupts = 0);
         printf("%03d Int Fail Count %3d No Int Fail %3d\n",i, fails_int, fails_no_int);
 
-        fails_no_int += LSeekStressTest(lock_interrupts = 1);
-        printf("%03d Int Fail Count %3d No Int Fail %3d\n",i, fails_int, fails_no_int);
         i += 1;
     }
 
@@ -52,81 +51,64 @@ rtems_task Init(
 }
 rtems_interrupt_lock_context lock_contex;
 rtems_interrupt_lock interrupt_lock;
-extern const unsigned char config_tar[];
-extern const size_t config_tar_size;
 
-void SetupFileSystem(){
-    if (config_tar_size > 0)
-    {
-        printf("Unpacking tar filesystem, this may take awhile...\n");
-        const unsigned char* ptr = config_tar;
-
-        if (Untar_FromMemory((char*) config_tar, config_tar_size) != 0)
-        {
-            printf("Can't unpack tar filesystem\n");
-            exit(1);
-        }
-    }
-}
-
-void do_some_work(int fd) {
-    char buffer[100];
-    lseek(fd, 0x2000, SEEK_SET); // Seek to a different offset
-    read(fd, buffer, sizeof(buffer)); // Read some data from the new offset
-}
-int LSeekStressTest(int lock_interrupts) {
+int64_t truth_divide[STRESS_COUNT];
+int64_t truth_mod[STRESS_COUNT];
+int64_t truth_mul[STRESS_COUNT];
+int Int64Test(int lock_interrupts) {
     int fd;
     uint32_t value;
-    char buffer[BUFFER_SIZE];
     int count = 0;
-    uint32_t EXPECTED_VALUE = 0x07e8;
     int fail_count = 0;
 
-    fd = open(FILENAME, O_RDONLY);
-    if (fd == -1) {
-        perror("Failed to open file");
-        exit(1);
-    }
-
+    volatile int64_t size;
+    volatile int64_t div_check;
+    volatile int64_t mod_check;
+    volatile int64_t mul_check;
     for (int i = 0; i < STRESS_COUNT; i++) {
+        size = TEST_OFFSET*i;
+
         if(lock_interrupts){
+
             rtems_interrupt_lock_acquire(interrupt_lock, &lock_contex);
-        }
-        int status = lseek(fd, TEST_OFFSET, SEEK_SET);
-        if (read(fd, buffer, BUFFER_SIZE) != BUFFER_SIZE) {
-            perror("Failed to read from file");
-            printf("status is %d, ftell is %d\n", status, lseek(fd, 0, SEEK_CUR));
-            close(fd);
-            exit(1);
+            div_check =  size / 128;
+            mod_check = size % 128;
+            mul_check = size * 128;
 
-        }
-
-        memcpy(&value, buffer, sizeof(value));
-        if(lock_interrupts){
+            truth_divide[i] = div_check;
+            truth_mod[i] = mod_check;
+            truth_mul[i] = mul_check;
             rtems_interrupt_lock_release(interrupt_lock, &lock_contex);
-        }    
-        /* Set expected value based off first load.*/
-        if(i == 0){
-          EXPECTED_VALUE = value;
-        }
 
-        if (value != EXPECTED_VALUE) {
-            printf("Mismatched value at iteration %d. Expected: 0x%x, Got: 0x%x : fpos %d\n",
-                   i, EXPECTED_VALUE, value,  lseek(fd, 0, SEEK_CUR));
-            fail_count++;
         }
-        /* Commenting out do_some_work out seems to make the problem happen less often.*/
-        do_some_work(fd);
-        count++;
-        memset(buffer, 0x39, sizeof(buffer));
+        else{
+            // rtems_interrupt_lock_acquire(interrupt_lock, &lock_contex);
+            div_check =  size / 128;
+            mod_check = size % 128;
+            mul_check = size * 128;
+            // rtems_interrupt_lock_release(interrupt_lock, &lock_contex);
+
+            if(truth_divide[i] != div_check){
+                printf("%8d div fail: truth %ld got %ld\n",i, truth_divide[i], div_check);
+                fail_count++;
+            }
+            if(truth_mod[i] != mod_check){
+                printf("%8d mod fail: truth %ld got %ld\n",i, truth_mod[i], mod_check);
+                fail_count++;
+            }
+            if(truth_mul[i] != mul_check){
+                printf("%8d mul fail: truth %ld got %ld\n",i, truth_mul[i], mul_check);
+                fail_count++;
+            }
+            count++;
+        }
 
     }
-    close(fd);
 
     if(fail_count== 0)
-        printf("Completed %d reads successfully\n", count);
+        printf("Completed %d ops successfully\n", count);
     else{
-        printf("Completed %d reads total fails at %d\n", count,fail_count);
+        printf("Completed %d ops total fails at %d\n", count,fail_count);
     }
 
     return fail_count;
--- make_test_file.py
+++ make_test_file.py
@@ -9,6 +9,7 @@ val = Meh()
 fid = open(filename,'wb')
 for i in range( int((1024*1024)*2.9/4)):
     val.value = i + 0xffff
+#    val.value = 0
     fid.write(val)
 fid.close()
 
